37969578953d7f3a4178c364966eb4380296d008
jho2301 <jho2301@gmail.com>
2021-04-03 11:37:42 +0900

    fix: PORT 환경변수로 설정

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "/dist/index.html"));
 });
 
-app.listen(5000, () => {
-  console.log("serve on http://localhost:5000");
+app.listen(process.env.PORT, () => {
+  console.log(`serve on http://localhost:${process.env.PORT}`);
 });
