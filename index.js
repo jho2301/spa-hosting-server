659f772f5f00a6ef753547146bb131da53a41bc5
hwano <jho2301@gmail.com>
2021-04-12 15:28:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import path from "path";
 
 const app = express();
 
-app.use("/", express.static(path.resolve("/dist")));
+app.use("/", express.static(path.resolve("dist")));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.resolve("/dist/index.html"));
+  res.sendFile(path.resolve("dist/index.html"));
 });
 
 app.listen(process.env.PORT, () => {
